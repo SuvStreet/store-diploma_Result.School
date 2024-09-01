43974794fa6ea9debef8c5fa70b08f91133b97eb
Dmitrij Kolotusha <suvstreet@gmail.com>
2024-09-01 17:21:26 +0300

    Добовление индикатора товаров в корзине

--- src/components/header/components/control-panel/ControlPanel.jsx
+++ src/components/header/components/control-panel/ControlPanel.jsx
@@ -1,18 +1,18 @@
 import PropType from 'prop-types'
 import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import { Icon } from '../../../icon/Icon'
+import { selectCart, selectUserId } from '../../../../redux/selectors'
 
 import { faHeart, faUser } from '@fortawesome/free-regular-svg-icons'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
-import { useSelector } from 'react-redux'
-import { selectUserId } from '../../../../redux/selectors'
-
 import styled from 'styled-components'
 
 const ControlPanelContainer = ({ className, userLogin }) => {
 	const navigate = useNavigate()
 	const userId = useSelector(selectUserId)
+	const cart = useSelector(selectCart)
 
 	const handleClickUser = () => {
 		navigate(!userLogin ? '/authorize' : `/profile/${userId}`)
@@ -29,6 +29,9 @@ const ControlPanelContainer = ({ className, userLogin }) => {
 			</div>
 			<div className='header-info__item' onClick={() => navigate('/cart')}>
 				<Icon iconCode={faCartShopping} />
+				{cart.items.length > 0 && <div className='volume'>
+					{cart.items.length}
+				</div>}
 			</div>
 		</div>
 	)
@@ -53,6 +56,25 @@ export const ControlPanel = styled(ControlPanelContainer)`
 		align-items: center;
 		padding: 0 5px;
 		cursor: pointer;
+		position: relative;
+	}
+
+	.volume{
+		position: absolute;
+		top: -5px;
+		right: 0;
+		display: flex;
+		font-weight: bold;
+		justify-content: center;
+		align-items: center;
+		padding: 0 5px;
+		min-width: 20px;
+		height: 20px;
+		font-size: 15px;
+		color: white;
+		background-color: red;
+		border-radius: 50%;
+		margin-left: 10px;
 	}
 `
 
